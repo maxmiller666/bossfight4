60a03a0c23a6caab38f7ff5b3591efd64271d037
maxmiller666 <23221@students.isb.ac.th>
2023-11-17 12:00:05 +0700

    idk

--- My project/Assets/EnemyBullet.cs
+++ My project/Assets/EnemyBullet.cs
@@ -8,14 +8,14 @@ public class EnemyBullet : MonoBehaviour
     private Rigidbody2D rb;
     public float force;
     public Transform tplayer;
-    public Vector3 targetPosition = new Vector3(32.58f, 0f, 0f);
+    public Vector3 targetPosition = new Vector3(32f, 0f, 0f);
     private bool shouldMove = true;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
 
         rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player");
@@ -32,7 +32,7 @@ public class EnemyBullet : MonoBehaviour
     {
         if (tplayer.position.x > 20f)
         {
-        
+
             shouldMove = false;
         }
         if (shouldMove)
--- My project/Assets/ghostfly.cs
+++ My project/Assets/ghostfly.cs
@@ -10,7 +10,7 @@ public class CircleMovement : MonoBehaviour
         // Calculate the new position based on the current time and speed
         float time = Time.time * 1;
         float x = Mathf.Cos(time) * 6;
-        float y = Mathf.Sin(time) * 3;
+        float y = Mathf.Sin(time) * 1;
 
         // Update the object's position with an offset for the circle's center
         transform.position = center + new Vector3(x, y, 0);
